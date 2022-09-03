a8cc6b101a4f69bc4982bb6a4518a9e99852ec3c
nicholas <nickkiim7@gmail.com>
2022-09-03 22:55:19 +0300

    timeline first function

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-// document.addEventListener(DOMLontentLoaded, init)
+(function () {
+    var items = document.querySelectorAll(".timeline li");
+    function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+})
+
 fetch("http://localhost:3000/days")
 .then((response) => response.json())
 .then((data) => console.log(data))
