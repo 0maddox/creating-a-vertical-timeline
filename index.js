e704bcabeba7d0e5304d587dd61d73f6dc2efa42
nicholas <nickkiim7@gmail.com>
2022-09-05 17:45:08 +0300

    f wip 1

--- index.js
+++ index.js
@@ -1,77 +1,43 @@
-  let arr = []
+ {let arr = []
    let daysString =``
-   const title =document.querySelector(`.titles-container`)
+   const title =document.querySelector(`.inview`)
     async function fetchData (){
     const response = await fetch("http://localhost:3000/days")
     arr = await response.json()
     arr.forEach(items =>{
-      daysString += `<span>${items.title}</span>`
+      daysString += `<div class ="titles-container">${items.id}, ${items.title}, ${items.description}</div>`
      })
      title.innerHTML = daysString
   }
   fetchData()
+}
 
-//   function displayDays(days){
-//     let dayDisplay= document.getElementById(`div`)
-//     for (let element of days){
-//       let li = document.createElement(`li`)
-//       li.className =`day`
-//       dayDisplay.appendChild(li)
-//     }
-//   }
-// (function () {
-//     var items = document.querySelectorAll(".timeline li");
-//     function isElementInViewport(el) {
-//     var rect = el.getBoundingClientRect();
-//     return (
-//       rect.top >= 0 &&
-//       rect.left >= 0 &&
-//       rect.bottom <=
-//         (window.innerHeight || document.documentElement.clientHeight) &&
-//       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//     );
-//   }
-// //callbacks
-//   function callbackFunc() {
-//     for (var i = 0; i < items.length; i++) {
-//       if (isElementInViewport(items[i])) {
-//         items[i].classList.add("in-view");
-//       }
-//     }
-//   }
-
-//   // event listeners
-//   window.addEventListener("load", callbackFunc);
-//   window.addEventListener("resize", callbackFunc);
-//   window.addEventListener("scroll", callbackFunc);
-// })()
- 
-// (function () {
-//   "use strict";
-//   var items = document.querySelectorAll(".timeline li");
-
-//     function isElementInViewport(el) {
-//     var rect = el.getBoundingClientRect();
-//     return (
-//       rect.top >= 0 &&
-//       rect.left >= 0 &&
-//       rect.bottom <=
-//         (window.innerHeight || document.documentElement.clientHeight) &&
-//       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//     );
-//   }
-
-//   function callbackFunc() {
-//     for (var i = 0; i < items.length; i++) {
-//       if (isElementInViewport(items[i])) {
-//         items[i].classList.add("in-view");
-//       }
-//     }
-//   }
-
-//   // listen for events
-//   window.addEventListener("load", callbackFunc);
-//   window.addEventListener("resize", callbackFunc);
-//   window.addEventListener("scroll", callbackFunc);
-// })();
-
+(function () {
+    "use strict";
+  
+    
+    let items = document.querySelectorAll(".timeline li");
+      function isElementInViewport(el) {
+      let rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  
+    function callbackFunc() {
+      for (let i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+          items[i].classList.add("in-view");
+        }
+      }
+    }
+  
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+  })();
+  
